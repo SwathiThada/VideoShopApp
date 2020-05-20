b5dde19ab1ebee1a4fd1b2d99fb7068afa740890
Swathi Thada <swathithada3@gmail.com>
2020-05-20 09:08:22 -0400

    Add project files.

---
+++ VideoShopApp/Controllers/MoviesController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Ajax.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VideoShopApp.Models;
+using VideoShopApp.ViewModels;
+
+namespace VideoShopApp.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Thappad" };
+            var customers = new List<Customer>
+            {
+                new Customer {Name = "Customer1"},
+                new Customer {Name = "Customer2"}
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+             
+            return View(viewModel);
+            //return PartialView(movie);
+            //return Content("Hello World!!");
+            //return HttpNotFound();
+            //return new EmptyResult();
+            //return RedirectToAction("Index", "Home");
+            
+        }
+        //public ActionResult Edit(int Id)
+        //{
+        //    return Content("id = " + Id);
+        //}
+        //public ActionResult Index(int? pageIndex, string sortBy)
+        //{
+        //    if (!pageIndex.HasValue)
+        //        pageIndex = 1;
+        //    if (String.IsNullOrWhiteSpace(sortBy))
+        //        sortBy = "Name";
+        //    return Content(String.Format("pageIndex ={0} & sortBy={1}",pageIndex, sortBy));
+
+        //}
+        [Route("movies/released")]
+        public ActionResult ByReleaseDate()
+        {
+            return Content("released in 2020");
+        }
+    }
+}
---
+++ VideoShopApp/ViewModels/RandomMovieViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using VideoShopApp.Models;
+
+namespace VideoShopApp.ViewModels
+{
+    public class RandomMovieViewModel
+    {
+        public Movie Movie { get; set; }
+        public List<Customer> Customers { get; set; }
+    }
+}
